ca1ffd55e14c97ee62f745826da034bbd413cf85
이창원 <gravity980530@gmail.com>
2026-04-12 23:10:00 +0900

    주석 추가

--- malloc-lab/mm.c
+++ malloc-lab/mm.c
@@ -33,13 +33,48 @@ team_t team = {
     ""
 };
 
+// 여기는 csapp에 나와있는 implicit(묵시적) 방법
+#define WSIZE 4 // 4바이트 사이즈
+#define DSIZE 8 // 8바이트 사이즈
+
+// 반환값 4096
+#define CHUNKSIZE (1<<12)
+
+// 크기 비교
+#define MAX(x, y) ((x) > (y)? (x) : (y)) 
+// ((size | alloc)) 반환 - size는 8단위고, 8단위 아래 3개는 비트 남으니까 alloc 상태값까지 더함.
+#define PACK(size, alloc) ((size) | (alloc))
+
+// p 양수로 읽기
+#define GET(p) (*(unsigned int *)(p))
+// p값 val로 바꾸기
+#define SET(p, val) (*(unsigned int *)(p) = (val))
+
+// 앰퍼샌드 == AND 연산자 | ~는 반대로 변환
+// 0x7 == 0000 0111 / ~0x7 == 1111 1000
+// 즉, 1111 1 을 시작지점으로 잡으면, 8이 몇 개 할당되었는지로 사이즈를 구할 수 있음.
+#define GET_SIZE(p) (GET(p) & ~0x7)
+// P의 첫 번째 자리 가져오기
+#define GET_ALLOC(p) (GET(p) & 0x1)
+
+// bp 블록 포인터 위치 주소
+// hdrp는 해당 bp의 헤드의 주소 (시작지점)
+#define HDRP(bp) ((char *)(bp) - WSIZE)
+// getSize는 헤당 블록의 최대 크기 + 페이로드 위치 = 4 + n - 8. 즉 푸터의 위치
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+
+// 다음 블럭의 페이로드 위치
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
+// 이전 블럭의
+#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
+// 아래는 naive malloc 구현방식 (동등한 같은 의미로 쓰임)
 /* 단일 워드(4) 또는 더블 워드(8) 정렬 */
-#define ALIGNMENT 8
+// #define ALIGNMENT 8
 
-/* ALIGNMENT의 배수로 올림 */
-#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
+// /* ALIGNMENT의 배수로 올림 */
+// #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
 
-#define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+// #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
 /*
  * mm_init - malloc 패키지를 초기화합니다.
