0f399f40797042fc8784dc78de38376bd4b3dee5
이창원 <gravity980530@gmail.com>
2026-04-14 19:59:24 +0900

    best fit 업데이트

--- malloc-lab/mm.c
+++ malloc-lab/mm.c
@@ -311,9 +311,20 @@ static void *find_fit(size_t asize) {
 static void *find_fit(size_t asize) {
     // 임시 temp선언
     char *bp;
-    char *simBp;
+    char *simBp = NULL;
 
-    for (bp = heap_listp; )
+    for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
+        if (!GET_ALLOC(HDRP(bp)) && (asize == GET_SIZE(HDRP(bp)))) {
+            return bp;
+        } else if (!GET_ALLOC(HDRP(bp)) && (asize < GET_SIZE(HDRP(bp)))) {
+            if (simBp == NULL || GET_SIZE(HDRP(bp)) < GET_SIZE(HDRP(simBp)))
+                simBp = bp;
+        }
+    }
+
+    if (simBp != NULL) {
+        return simBp;
+    }
 
     return NULL;
 }
